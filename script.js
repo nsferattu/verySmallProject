4b3b68aaa378e84385702d4481e0abc2591538f5
nsferattu <bb1sixx@gmail.com>
2025-12-02 19:36:55 +0100

    Bla

--- script.js
+++ script.js
@@ -5,13 +5,13 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-// Function declaration of open modal
+// Open modal function
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-// Function declaration of close modal
+// Close modal function
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
